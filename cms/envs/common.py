f987dabe68b61b17c3b32b938383082f4745061b
ichuang <ichuang@mit.edu>
2013-08-16 16:23:47 -0400

    add push_to_lms to Studio; uses MITX_FEATURES['ENABLE_PUSH_TO_LMS']

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -62,6 +62,9 @@ FEATURES = {
     # If set to True, new Studio users won't be able to author courses unless
     # edX has explicitly added them to the course creator group.
     'ENABLE_CREATOR_GROUP': False,
+
+    # Enable push to LMS (export of studio course to github)
+    'ENABLE_PUSH_TO_LMS': False,
 }
 ENABLE_JASMINE = False
 
