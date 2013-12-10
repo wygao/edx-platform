31a1f492d10ede7107c8d0ea24a53ec868395db5
Oleg Marshev <oleh.marshev@gmail.com>
2013-12-10 15:54:28 +0200

    Fix tests.

--- common/lib/xmodule/xmodule/tests/test_lti_unit.py
+++ common/lib/xmodule/xmodule/tests/test_lti_unit.py
@@ -312,7 +312,7 @@ class LTIModuleTest(LogicTest):
         Test if OAuth signing was successful.
         """
         try:
-            self.xmodule.verify_oauth_body_sign(self.get_signed_mock_request())
+            self.xmodule.verify_oauth_body_sign(self.get_signed_grade_mock_request())
         except LTIError:
             self.fail("verify_oauth_body_sign() raised LTIError unexpectedly!")
 
@@ -323,7 +323,7 @@ class LTIModuleTest(LogicTest):
         Oauth signing verify fail.
         """
         with self.assertRaises(LTIError):
-            req = self.get_signed_mock_request()
+            req = self.get_signed_grade_mock_request()
             self.xmodule.verify_oauth_body_sign(req)
 
     def get_signed_grade_mock_request(self):
